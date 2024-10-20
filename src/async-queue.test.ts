e882d321c56eede3e288095f9f1a89429f7708fe
markg <17348049+sockcrates@users.noreply.github.com>
2024-10-20 17:19:05 -0400

    Create test suite and initial boilerplate

---
+++ src/async-queue.test.ts
@@ -0,0 +1,87 @@
+import { describe, expect, it, vi } from 'vitest';
+import { AsyncQueue } from './async-queue';
+
+function makeTask<TValue>(value: TValue, timeoutMs: number) {
+  return () =>
+    new Promise<TValue>((resolve) => {
+      setTimeout(() => {
+        resolve(value);
+      }, timeoutMs);
+    });
+}
+
+describe('asyncQueue', () => {
+  it('processes a task', () => {
+    const callback = vi.fn();
+    const queue = new AsyncQueue();
+    const task = (): Promise<number> => Promise.resolve(1);
+    queue.enqueue(task).catch((_: unknown) => {
+      expect.unreachable();
+    });
+    expect.poll(() => {
+      expect(callback).toHaveBeenCalledWith(1);
+    });
+  });
+  it.skip('does not exceed the concurrency limit', async () => {
+    const queue = new AsyncQueue(3);
+    let runningTasks = 0;
+    let maxConcurrent = 0;
+    const tasks = Array.from(
+      { length: 10 },
+      () => () =>
+        new Promise<void>((resolve) => {
+          runningTasks += 1;
+          maxConcurrent = Math.max(maxConcurrent, runningTasks);
+          setTimeout(() => {
+            runningTasks -= 1;
+            resolve();
+          }, 100);
+        }),
+    );
+    await Promise.all(tasks.map((task) => queue.enqueue(task)));
+    expect(maxConcurrent).toEqual(3);
+  });
+  it.skip('executes tasks in the order they were added', () => {
+    const callback = vi.fn();
+    const queue = new AsyncQueue(3);
+    const tasks = Array.from({ length: 5 }, (_, index) =>
+      makeTask(index + 1, (index + 1) * 100),
+    );
+    tasks.forEach((task) => {
+      queue.enqueue(task, { callback }).catch((_: unknown) => {
+        expect.unreachable();
+      });
+    });
+    expect.poll(() => {
+      expect(callback.mock.calls.map((call: number[]) => call[0])).toEqual([
+        1, 2, 3, 4, 5,
+      ]);
+    });
+  });
+  it.skip('handles task rejection and continues processing others', () => {
+    const callback = vi.fn();
+    const callbackError = vi.fn();
+    const queue = new AsyncQueue(3);
+    const tasks = [1, 2, 3, 4, 5].map(
+      (num) => () =>
+        new Promise<number>((resolve, reject) => {
+          if (num === 3) {
+            reject(new Error('Task failed'));
+          } else {
+            resolve(num);
+          }
+        }),
+    );
+    tasks.forEach((task) => {
+      queue.enqueue(task, { callback, callbackError }).catch((_: unknown) => {
+        expect.unreachable();
+      });
+    });
+    expect.poll(() => {
+      expect(callback.mock.calls.map((call: number[]) => call[0])).toEqual([
+        1, 2, 4, 5,
+      ]);
+      expect(callbackError).toHaveBeenCalledWith(3);
+    });
+  });
+});
---
+++ src/async-queue.ts
@@ -0,0 +1,23 @@
+interface Options {
+  callback?: <TData>(result: TData) => void;
+  callbackError?: (error: unknown) => void;
+}
+
+type TaskFactory<TData> = () => Promise<TData>;
+
+interface Request<TData> {
+  options?: Options;
+  task: TaskFactory<TData>;
+}
+
+export class AsyncQueue {
+  #queue: Request<unknown>[] = [];
+  #workers: number;
+  constructor(workers = 3) {
+    this.#workers = workers;
+  }
+  enqueue<TData>(
+    task: TaskFactory<TData>,
+    options: Options = {},
+  ): Promise<void> {}
+}
